d26680bdf3d9e1f3af5242731283db1311891535
Ali-Reza Mosavi <amosavi@cs.ualberta.ca>
2019-11-01 15:03:25 -0600

    test

--- app/src/test/java/com/example/simpleparadox/listycity/CityListTest.java
+++ app/src/test/java/com/example/simpleparadox/listycity/CityListTest.java
@@ -93,4 +93,10 @@ class CityListTest {
 
         assertEquals(1, cityList.countCities());
     }
+    @Test
+    void failedTest() {
+        CityList cityList = mockCityList();
+
+        assertEquals(1, 2);
+    }
 }
